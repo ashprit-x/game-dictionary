39759e7593b3d96d00299d9b0a8541a08b3efe32
ashprit <ashpritmehra@gmail.com>
2023-12-29 13:13:48 +0000

    Added config module to prevent API access

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { apiKey } from './config'
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
-        key: '2a1afaf8dc694a31a2adfb2fde431316'
+        key: apiKey
     }
 })
