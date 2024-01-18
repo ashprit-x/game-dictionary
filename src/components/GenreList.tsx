a3374a8311c575ae18f0357f1e2dc153c810df4b
ashprit <ashpritmehra@gmail.com>
2024-01-18 19:57:37 +0000

    fixed all console errors

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
+import { Button, HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
@@ -14,7 +14,7 @@ const GenreList = () => {
         <ListItem key={genre.id} paddingY={'5px'}>
           <HStack>
             <Image boxSize={'32px'} borderRadius={'8'} src = {getCroppedImageUrl(genre.image_background)}/>
-            <Text fontSize={'lg'}>{genre.name}</Text>
+            <Button onClick={(() => console.log(genre))} fontSize={'lg'} variant={"link"}>{genre.name}</Button>
             </HStack>
         </ListItem>
       ))}
