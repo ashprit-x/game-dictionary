a2d1e6ab74e686f1e9be3fe828f5390733363e9c
ashprit <ashpritmehra@gmail.com>
2024-01-18 19:43:34 +0000

    genres displayed and fixed ux errors

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,16 @@ function App() {
   return <Grid templateAreas={{
     base: `"nav" "main"`,
     lg: `"nav nav" "aside main"` // large devices -> 1024px
+  }}
+  templateColumns={{
+    base: '1fr',
+    lg: '200px 1fr'
   }}>
     <GridItem area='nav'>
       <NavBar/>
     </GridItem>
     <Show above="lg">
-      <GridItem area='aside' >
+      <GridItem area='aside'paddingX={5} >
         <GenreList/>
       </GridItem>
     </Show>
