6c131240b888781804eb7bc41b79b407e8af7ae1
ashprit <ashpritmehra@gmail.com>
2024-01-23 21:31:19 +0000

    fixed image issue & added sort by feature

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,6 @@
+import noImage from '../assets/no-image-placeholder-6f3882e0.webp'
 const getCroppedImageUrl = (url: string) =>{
+    if(!url) return noImage;
     const target = 'media/';
     const idx = url.indexOf(target)+target.length;
     return url.slice(0,idx) + 'crop/600/400/' + url.slice(idx);
